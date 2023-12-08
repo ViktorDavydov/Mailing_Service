7515043ec815d81c82aaee60038b73974d6cf615
ViktorDavydov <davydov.viktor.andreevich@gmail.com>
2023-12-09 00:11:51 +0300

    models creating commit

--- mailer/admin.py
+++ mailer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailer.models import Client
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('client_name', 'client_email',)
+    list_filter = ('client_name',)
+    search_fields = ('client_name',)
--- mailer/models.py
+++ mailer/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Client(models.Model):
+    client_email = models.CharField(max_length=30, verbose_name="контактный email")
+    client_name = models.CharField(max_length=50, verbose_name="ФИО")
+    comment = models.TextField(verbose_name="комментарий", **NULLABLE)
+
+    def __str__(self):
+        return f"{self.client_name} ({self.client_email})"
+
+    class META:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
+        ordering = ('client_name',)
+
