e0a3f60af0fc6cf9b94b57f7ba8401c8624bfe0c
ViktorDavydov <davydov.viktor.andreevich@gmail.com>
2023-12-09 20:15:40 +0300

    first models done commit

--- mailer/admin.py
+++ mailer/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from mailer.models import Client
+from mailer.models import Client, SendOptions, SendText
 
 
 @admin.register(Client)
@@ -8,3 +8,17 @@ class ClientAdmin(admin.ModelAdmin):
     list_display = ('client_name', 'client_email',)
     list_filter = ('client_name',)
     search_fields = ('client_name',)
+
+
+@admin.register(SendOptions)
+class SendOptionsAdmin(admin.ModelAdmin):
+    list_display = ('send_time', 'send_period', 'send_status',)
+    list_filter = ('send_status',)
+    search_fields = ('send_status',)
+
+
+@admin.register(SendText)
+class SendTextAdmin(admin.ModelAdmin):
+    list_display = ('mail_title', )
+    list_filter = ('mail_title',)
+    search_fields = ('mail_title',)
--- mailer/models.py
+++ mailer/models.py
@@ -11,8 +11,50 @@ class Client(models.Model):
     def __str__(self):
         return f"{self.client_name} ({self.client_email})"
 
-    class META:
+    class Meta:
         verbose_name = 'клиент'
         verbose_name_plural = 'клиенты'
         ordering = ('client_name',)
 
+
+class SendOptions(models.Model):
+    send_time = models.DateTimeField(auto_now=False, auto_now_add=False,
+                                     verbose_name='время рассылки')
+    send_period = models.CharField(max_length=20, verbose_name='периодичность')
+    send_status = models.CharField(max_length=20, verbose_name='статус рассылки')
+
+    def __str__(self):
+        return f"{self.send_period} - {self.send_status}"
+
+    class Meta:
+        verbose_name = 'настройка'
+        verbose_name_plural = 'настройки'
+        ordering = ('send_time',)
+
+
+class SendText(models.Model):
+    mail_title = models.CharField(max_length=100, verbose_name='тема')
+    mail_text = models.TextField(verbose_name='сообщение')
+
+    def __str__(self):
+        return f"{self.mail_title}"
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'сообщения'
+        ordering = ('mail_title',)
+
+
+class Logs(models.Model):
+    last_try = models.DateTimeField(auto_now=False, auto_now_add=False,
+                                    verbose_name='дата и время последней попытки')
+    status_try = models.BooleanField(verbose_name='статус попытки')
+    server_answer = models.TextField(verbose_name='ответ сервера', **NULLABLE)
+
+    def __str__(self):
+        return f"{self.status_try}: {self.last_try}"
+
+    class Meta:
+        verbose_name = 'лог'
+        verbose_name_plural = 'логи'
+        ordering = ('last_try',)
